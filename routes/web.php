06d04b50f37773fb77920f68ee7b52b953e4871e
yxj0312 <yanxiajun@hotmail.com>
2024-01-05 22:32:01 +0100

    add secert

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,22 @@
 <?php
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    Http::post('https://api.openai.com/v1/chat/completions', [
+        "model" => "gpt-3.5-turbo",
+        "messages" => [
+            [
+                "role" => "system",
+                "content" => "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."
+            ],
+            [
+                "role" => "user",
+                "content" => "Compose a poem that explains the concept of recursion in programming."
+            ]
+        ]
+    ])->json();
+
+    
 });
