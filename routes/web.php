9bae8abd1886cfc8af6cc53fc6996c722ffcb674
yxj0312 <yanxiajun@hotmail.com>
2024-01-16 22:26:14 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     $chat = new Chat();
 
-    $poem = $chat
+    $chat
         ->systemMessage("You are a poetic assistant, skilled in explaining complex programming concepts with creative flair.")
         ->send('Compose a poem that explains the concept of recursion in programming.');
 
     $sillyPoem = $chat->reply('Cool, can you make it much, much sillier.');
+
+
 
     return view('welcome', ['poem' => $sillyPoem]);
 });
